0bbd43ce48288633cfd826564af3c63d127dd746
Paul Defilippi <BlindFaith@pauls-imac.home>
2016-10-04 23:40:09 -0400

    Installed Firebase, Installed GeoFire, created objective-c .h Bridging Header file

---
+++ WhereisPoke/WhereIsPoke-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  WhereIsPoke-Bridging-Header.h
+//  WhereisPoke
+//
+//  Created by Paul Defilippi on 10/4/16.
+//  Copyright © 2016 Paul Defilippi. All rights reserved.
+//
+
+#import "GeoFire.h"
